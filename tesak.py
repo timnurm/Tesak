8624384d04b06e5289f561fcfaa11cb0ba909260
Tim <60013754+timnurm@users.noreply.github.com>
2021-06-18 07:15:14 +0300

    Add files via upload

---
+++ tesak.py
@@ -0,0 +1,17 @@
+name = input('Как тебя зовут?: ')
+
+print(name + ", ты похож на пидораса")
+print(name + ', откуда ты?')
+city = input("Укажите ваше местоположение: ")
+print(city + '? ты третий пидорас из '+ city + ' которого я ловлю')
+
+print(name + ', хуй сосешь?')
+statuspidor = input('Да/нет: ')
+
+print(statuspidor + "?\nмы похожи на дебилов?")
+foolstatus = input("Да/нет:")
+
+print(name + ', тебя в детстве ебли?')
+childhood = input("Да/нет,не ебли :")
+print('поражение \n...\nвас облили мочой\ndialogue end')
+
